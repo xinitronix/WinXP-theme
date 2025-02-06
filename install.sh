27975c25b3cb282d6a6eae6f6b570d2ec9b7caa9
Your Name <you@example.com>
2025-02-06 22:16:15 +0300

    новый файл:    git.sh 	новый файл:    icons/BAD_power_off.png 	новый файл:    icons/appearence.ico 	новый файл:    icons/close.png

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p ~/winxp
+cp -r win  ~/winxp
+cp -r icons  ~/winxp
+cp tint2/tint2rc ~/.config/tint2
+mkdir -p ~/.config/rofi
+cp rofi/config.rasi ~/.config/rofi
+cp rofi/win_xp.rasi ~/.config/rofi
