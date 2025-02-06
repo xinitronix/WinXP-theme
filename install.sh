f4f79db84fd61bf18ae11c03a92207d6a388b414
Your Name <you@example.com>
2025-02-06 22:34:10 +0300

    изменено:      install.sh 	новый файл:    pcmanfm/default/desktop-items-0.conf 	новый файл:    pcmanfm/default/pcmanfm.conf 	новый файл:    xinitrc

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ cp tint2/tint2rc ~/.config/tint2
 mkdir -p ~/.config/rofi
 cp rofi/config.rasi ~/.config/rofi
 cp rofi/win_xp.rasi ~/.config/rofi
+cp 1.jpg  ~/winxp
+cp -r pcmanfm ~/.config
+cp xinitrc ~/.xinitrc
